41602d884c6aee8a73818ece3a1c0d7d0a4b5de4
Levi Jones <leviwill.j@gmail.com>
2024-10-19 00:17:35 -0600

    insert country name into amazon DOM

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,11 @@
+function createBubble (country) {
+  const bubble = document.createElement('div');
+  const amazonCoreprice = document.getElementById('corePrice_desktop');
+  bubble.classList.add('bubble');
+  bubble.innerText = country;
+  amazonCoreprice.insertAdjacentElement('afterend', bubble);
+}
+
 const getCountryOfOrigin = async (asin) => {
   // It's not stealing if they don't know you're doing it
   const data = await fetch('https://www.wecultivate.us/snippet/amazon-search-page/get-origins-from-feedback', {
@@ -11,13 +19,24 @@ const getCountryOfOrigin = async (asin) => {
     })
   });
   return await data.json();
-  // document.getElementById('output').innerText = JSON.stringify(json);
 };
 
 chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
-    const asin = tab.url.split('/')[5];
-    const res = await getCountryOfOrigin(asin);
-    console.log(res);
+    const split = tab.url.split('/');
+    const asin = split[5];
+
+    if (split[2] === 'www.amazon.com' && split[4] === 'dp' && asin) {
+      const res = await getCountryOfOrigin(asin);
+      const countryName = res[0].countryName;
+
+      console.log(countryName);
+
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        function: createBubble,
+        args: [countryName],
+      });
+    }
   }
 });
